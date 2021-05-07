514fba74022efc12f56bbd7ca6ac9685fef53f06
Alicja Spiewak <aliszjaspiewak@gmail.com>
2021-05-07 15:16:40 +0200

    js

--- main.js
+++ main.js
@@ -1,51 +1,3 @@
-function wyslij() {
-    let formData = []
-    const imie = document.getElementById('imie').value
-    if(imie.lenght < 3) {
-         document.getElementById('imie-error').style = 'visibility: visible;'
-        } else {
-            formData.push(imie)
-            document.getElementById('imie-error').style = 'visibility: hidden;'
-        }
-    const nazwisko = document.getElementById('nazwisko').value
-    if(nazwisko.lenght < 4) {
-            document.getElementById('nazwisko-error').style = 'visibility: visible;'
-        } else {
-            formData.push(nazwisko)
-            document.getElementById('nazwisko-error').style = 'visibility: hidden;'
-        }
-    const email = document.getElementById('email').value
-    const tel = document.getElementById('tel').value
-    const wiad = document.getElementById('wiad').value
-    console.log('imie:', imie,'nazwisko:', nazwisko,'email:', email,'telefon:', tel,'wiadomosc:', wiad)
-}
-
-function klik() {
-    const imie = document.getElementById('imie').value
-    if(imie.length < 3) {
-        alert ("za krótkie imię")
-    }
-    const nazwisko = document.getElementById('nazwisko').value
-    if(nazwisko.length < 5) {
-        alert ("wpisz adres")
-    }
-    const wiad = document.getElementById('wiad').value
-    if(imie.length < 10) {
-        alert ("za krótka wiadomość")
-    }
-}
-
-function pobieranie(val){
-    console.log(val)
-}
-
-function pizza(pizza) {
-    const wybranaPizza = pizza
-    console.log(wybranaPizza)
-}
-
-//---------------------------------------------------
-
 // function wyslij() {
 //     let formData = []
 //     const imie = document.getElementById('imie').value
@@ -69,4 +21,48 @@ function pizza(pizza) {
         
 //     }
 // }
+
+function imie() {
+    const imie = document.getElementById('imie').value
+    if(imie.length < 2) {
+        alert('Wpisz imi')
+    } else 
+    console.log(imie)
+}
+
+function nazwisko() {
+    const nazwisko = document.getElementById('nazwisko').value
+    if(nazwisko.length < 3) {
+        alert('Wpisz nazwisko')
+    }else
+    console.log(nazwisko)
+}
+
+function wiad() {
+    const wiad = document.getElementById('wiad').value
+    if(wiad.length < 10) {
+        alert('Za krótka wiadomość')
+    }else
+    console.log(wiad)
+}
+ 
+function tel(val) {
+    const arr = val.split('')
+    const num = arr.filter(item => {
+        if(item === '1' || item === '2' || item === '3' || item === '4' || item === '5' || item === '6' || item === '7' || item === '8' || item === '9') {
+            return item
+        } else {
+             alert("Tylko cyfry")
+        }return
+    })
+    console.log(num)
+}
+
+function email() {
+    const email = document.getElementById('email').value
+    if(email.search('@') === -1) {
+    alert('niepoprawny email')}
+    else{
+    console.log(email)}
+}
 
